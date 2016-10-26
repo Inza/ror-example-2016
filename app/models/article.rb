ba38179b1be4195ce2e7ac49a3a3b0ae16dc085f
Tomas Jukin <tomas.jukin@juicymo.cz>
2016-10-26 15:18:26 +0200

    Add annotate gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :integer          not null, primary key
+#  text         :text
+#  title        :string
+#  is_published :boolean
+#  published_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Article < ActiveRecord::Base
 end
